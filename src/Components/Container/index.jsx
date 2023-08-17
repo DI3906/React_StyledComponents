708551203b45d450e4a2d290b136cc565e8ff59c
Diego Garcia <diegg066.99@gmail.com>
2023-08-17 14:39:12 -0600

    Clase terminada (React: Utilizando Styled Components)

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-
 import Title from "../Title";
 import Account from "../Account";
+import { styled } from "styled-components";
+import List from "../List";
+
+const StyleConteiner = styled.div`
+  background-color: ${({theme}) => theme.body};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StyleContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
 
 const Container = () => {
   return (
-    <div className="container">
+    <StyleConteiner >
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyleContent>
         <Account />
-      </section>
-    </div>
+        <List/>
+      </StyleContent>
+    </StyleConteiner>
   );
 };
 
